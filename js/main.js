416c6a7b66bc3140d00c52880d031ef57cde911c
gumeeee <guibr406@gmail.com>
2023-05-24 16:16:14 -0300

    Criando uma function para o pom e colocando o atributo onclick na tecla pom

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-document.querySelector('.tecla_pom');
+//Function para ativar o som das teclas
+function tocaSomPom() {
+  document.querySelector('#som_tecla_pom').play();
+} 
 
-
+//Eventos para ativar o som das teclas
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
